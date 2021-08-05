01fa823d87ccc0d9df072ee883095e330844a43c
Mohamed Abdelhamed <77297528+1998mohamed@users.noreply.github.com>
2021-08-05 02:10:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import React, { useState, useEffect } from 'react'
+
 import './App.css';
 import Login from './components/Login';
-import Home from './components/home';
+import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Switch, Route, Router} from "react-router-dom";
-import ClipLoader from "react-spinners/ClipLoader";
 
 
 function App() {
 
-  const  [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    },5000)
-  },[]);
-
   return (
     <div >
-      {
-        loading ?    
-        <div className="spinner"><ClipLoader color={"#000000"} loading={loading} size={150} /></div>
-        :
+      
         <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/home" component={Home} />
       </Switch>
-      }
     </div>
   );
 }
